c8087d62260f4446ac89af9c8e284880f87d97a6
miver33 <miver33@mail.ru>
2022-12-22 18:50:47 +0500

    исправили int 29

--- Abramyan/Integers/int_29.py
+++ Abramyan/Integers/int_29.py
@@ -1,9 +1,9 @@
-a: int = int(input("enter a"))
-b: int = int(input("enter b"))
-c: int = int(input("enter c"))
+a: int = int(input("enter a "))
+b: int = int(input("enter b "))
+c: int = int(input("enter c "))
 
 rectangle_area: int = a * b
 square_area: int = c * c
-number_of_square: int = rectangle_area // square_area
+number_of_square: int = (a // c) * (b // c)
 
 print(f"number of squares = {number_of_square}")
