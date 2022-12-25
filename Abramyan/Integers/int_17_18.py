eafe8aeaf26731173017672cb21bb19f8dad8a0c
miver33 <miver33@mail.ru>
2022-12-25 13:32:45 +0500

    доделал долги Абрамян

--- Abramyan/Integers/int_17_18.py
+++ Abramyan/Integers/int_17_18.py
@@ -1,6 +1,9 @@
-number: int = int(input("enter three-digit number: "))
+number: int = int(input("enter four-digit number: "))
 
 num1: int = (number // 100)
-num2: int = number % 10
+num2: int = num1 % 10
+num3: int = number // 1000
+num4: int = num3 % 10
 
-
+print("Цифра в сотне: ", num2)
+print("цифра в тысяче", num4)
--- Abramyan/Integers/int_19_23.py
+++ Abramyan/Integers/int_19_23.py
@@ -1,7 +1,14 @@
 number: int = int(input("enter number of seconds: "))
 
 m: int = int(number // 60)
-h = int(number // 60 // 60)
+h: int = int(number // 60 // 60)
+last_second_of_minutes: int = (number % 60)
+last_second_of_hour: int = number % 3600
+minutes_from_hour: int = m % 60
+
 
 print(f"minutes = {m}")
 print(f"hours = {h}")
+print(f"секунды с последней минуты {last_second_of_minutes}")
+print(f"секунды с последнего часа {last_second_of_hour}")
+print(f"минуты с последнего часа {minutes_from_hour}")
